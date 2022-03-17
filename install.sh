c85e1629c05afe87b9f09d729f706fd7b5c7ae71
Mohammd Reza Mollasalehi <49244782+mm3906078@users.noreply.github.com>
2022-03-17 22:24:27 +0330

    Create install.sh
    initial commit

---
+++ install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+apt install -y python3-pip python3
+modprobe iptable_nat
+echo 1 > /proc/sys/net/ipv4/ip_forward
+touch /etc/systemd/system/vgate.service
+cat > /etc/systemd/system/vgate.service <<'EOF'
+[Unit]
+Description=Virtual Gate
+After=multi-user.target
+[Service]
+Type=simple
+Restart=always
+ExecStart=/bin/bash -c 'cd /opt/vGate/ && python3 main.py'
+RestartSec=1
+StartLimitInterval=0
+[Install]
+WantedBy=multi-user.target
+EOF
